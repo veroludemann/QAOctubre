b17ee3cbf9b3250086de5582f4143e56fc7e6500
FacundoPasqua <facundo.pasqua@santexgroup.com>
2025-10-23 18:50:06 -0300

    Clase 4

--- cypress/e2e/miPrimerTest.cy.js
+++ cypress/e2e/miPrimerTest.cy.js
@@ -1,29 +1,71 @@
+// Funciones para generar datos aleatorios
+function generarEmailRandom() {
+  const tiempoActual = Date.now();
+  return `test${tiempoActual}@gmail.com`;
+}
+
+function generarDNIRandom() {
+  // Genera un DNI de 7-8 dígitos
+  const dni = Math.floor(Math.random() * 90000000) + 10000000;
+  return dni.toString();
+}
+
+function generarTelefonoRandom() {
+  // Genera un Numero de Telefono de 10 dígitos
+  const numeroTelefono = Math.floor(Math.random() * 9000000000) + 1000000000;
+  return numeroTelefono.toString();
+}
+/* {testIsolation: false},  (va despues de la coma y antes del parentesis) */ 
 describe('Registro de Usuario', () => {
-
-
-  it('Happy Path', () => {
-   cy.visit('http://localhost:3000/auth/registerUser')
-   cy.get('[data-cy="input-nombres"]').type('Juan Carlos')
-   cy.get('[data-cy="input-apellido"]').type('Perez Gomez')
-   cy.get('[data-cy="input-telefono"]').type('1126666931')
-   cy.get('[data-cy="input-dni"]').type('11266669') 
-   cy.get('[data-cy="select-provincia"]').type('Córdoba')
-   cy.press(Cypress.Keyboard.Keys.ENTER)
-   cy.get('[data-cy="select-localidad"]').type('Santa Rosa de Calamuchita')
-   cy.press(Cypress.Keyboard.Keys.ENTER)
-   
-   cy.get('[data-type="day"]').type('14')
-   cy.get('[data-type="month"]').type('01')
-   cy.get('[data-type="year"]').type('1997')
-   cy.get('[data-cy="input-email"]').type('facun123123@gmail.com')
-   cy.get('[data-cy="input-confirmar-email"]').type('facun123123@gmail.com')
-  cy.get('[data-cy="input-password"]').type('Test1234!')
-  cy.get('[data-cy="input-repetir-password"]').type('Test1234!')
-  cy.get('[data-cy="btn-registrarse"]').click()
-  cy.wait(1000)
-  cy.get('[data-cy="input-nombres"]').should('have.value', 'Juan Carlos')
+beforeEach(() => {
+    cy.visit('https://ticketazo.com.ar/auth/registerUser')
+}) 
+  it.skip('Happy Path', () => {
+    // Generar datos aleatorios para cada ejecución
+    const emailRandom = generarEmailRandom();
+    const dniRandom = generarDNIRandom();
+    const telefonoRandom = generarTelefonoRandom();
+    cy.log(emailRandom)
+    cy.log(dniRandom)
+    cy.log(telefonoRandom)
+    cy.get('[data-cy="input-nombres"]').type('Juan Carlos')
+    cy.get('[data-cy="input-apellido"]').type('Perez Gomez')
+    cy.get('[data-cy="input-telefono"]').type(telefonoRandom)
+    cy.get('[data-cy="input-dni"]').type(dniRandom) 
+    cy.get('[data-cy="select-provincia"]').type('Córdoba{enter}')
+    cy.get('[data-cy="select-localidad"]').type('Santa Rosa de Calamuchita{enter}')
+    cy.get('[data-type="day"]').type('14')
+    cy.get('[data-type="month"]').type('01')
+    cy.get('[data-type="year"]').type('1997')
+    cy.get('[data-cy="input-email"]').type(emailRandom)
+    cy.get('[data-cy="input-confirmar-email"]').type(emailRandom)
+    cy.get('[data-cy="input-password"]').type('Test1234!')
+    cy.get('[data-cy="input-repetir-password"]').type('Test1234!')
+    cy.get('[data-cy="btn-registrarse"]').click()
   })
 
+  it('Wrong Path', () => {
+    const emailRandom = generarEmailRandom();
+    const dniRandom = generarDNIRandom();
+    const telefonoRandom = generarTelefonoRandom();
+    cy.get('[data-cy="btn-registrarse"]').click()
+    cy.contains('Please fill out this field.').should('be.visible')
+    cy.get('[data-cy="input-nombres"]').type('Juan Carlos')
+    cy.get('[data-cy="input-apellido"]').type('Perez Gomez')
+    cy.get('[data-cy="input-telefono"]').type(telefonoRandom)
+    cy.get('[data-cy="input-dni"]').type(dniRandom) 
+    cy.get('[data-cy="select-provincia"]').type('Córdoba{enter}')
+    cy.get('[data-cy="select-localidad"]').type('Santa Rosa de Calamuchita{enter}')
+    cy.get('[data-type="day"]').type('14')
+    cy.get('[data-type="month"]').type('01')
+    cy.get('[data-type="year"]').type('1997')
+    cy.get('[data-cy="input-email"]').type('facundopasqua@gmail.com')
+    cy.get('[data-cy="input-confirmar-email"]').type('facundopasqua@gmail.com')
+    cy.get('[data-cy="input-password"]').type('Test1234!')
+    cy.get('[data-cy="input-repetir-password"]').type('Test1234!')
+    cy.get('[data-cy="btn-registrarse"]').click()
+    cy.get('[data-cy="error-message"]').contains('Ya existe un usuario registrado con ese correo electrónico').should('be.visible')
+  })
 
   
   
